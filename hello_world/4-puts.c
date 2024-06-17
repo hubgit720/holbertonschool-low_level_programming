d2e67d5e94449b824e5fbaa325b96489ba4eeb2f
hubgit720 <n.hamzaoui@afept.fr>
2024-06-17 09:46:33 +0000

    answer task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -o puzzle puzzle.c
+#include <stdio.h>
+/**
+* main - Entry point
+* Return:0
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+	return (0);
+}
