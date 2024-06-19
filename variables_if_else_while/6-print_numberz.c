8041c147440396e479634a262ae0e5ce0eb3115b
hubgit720 <n.hamzaoui@afept.fr>
2024-06-19 12:29:09 +0000

    answer task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Write a program that prints all single digi
+* numbers of base 10 starting from 0,
+* Return: Always
+*/
+int main(void)
+{
+	int numbers;
+
+for (numbers = '0'; numbers <= '9'; numbers++)
+{
+putchar(numbers);
+}
+for (numbers = '0'; numbers <= '9'; numbers++)
+putchar('\n');
+return (0);
+
+}
