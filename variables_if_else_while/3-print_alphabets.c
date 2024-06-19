11f5757264e1552ec4c690de91fe8a2d1358e566
hubgit720 <n.hamzaoui@afept.fr>
2024-06-19 09:01:53 +0000

    answer task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,10 @@ char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+putchar(alphabet);
 }
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+{
 putchar(alphabet);
 }
 putchar('\n');
