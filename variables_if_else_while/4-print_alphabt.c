271d1c34ba5abaae90833d5436f8459df4969e8d
hubgit720 <n.hamzaoui@afept.fr>
2024-06-19 10:07:17 +0000

    answer task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
-* main - Write a program that prints the alphabet in
-* lowercase,and then in uppercase, followed by a new line
+* main - Write a program that prints the alphabet in lowercase, followed by a new line.
+*
 * Return: Always
 */
 int main(void)
 {
-char alphabet;
+	char alphabet;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-{
-putchar(alphabet);
-}
-putchar('\n');
-return (0);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
 
+
+
+	putchar('\n');
+	return (0);
+}
