10719a5de24f5cd042b79cbb560ed53aa8a22337
hubgit720 <n.hamzaoui@afept.fr>
2024-06-19 10:14:16 +0000

    answer task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - Write a program that prints the alphabet in lowercase, followed by a new line.
+* main - Write a program that prints the alphabet in lowercase, followed by a n
 *
 * Return: Always
 */
