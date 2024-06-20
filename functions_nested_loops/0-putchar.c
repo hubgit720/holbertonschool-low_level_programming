5ec3af743c0a87218eecec07f108b7d4abe9b7f2
hubgit720 <n.hamzaoui@afept.fr>
2024-06-20 13:42:31 +0000

    answer task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-_putchar("_");
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -14,7 +14,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-{
+
 return (0);
 
 }
