cc6aa793d742f3f5a9ee684d66b26907324f3de3
hubgit720 <n.hamzaoui@afept.fr>
2024-06-20 13:58:48 +0000

    test1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar
+_putchar('\n');
+return (0);
+
+}
