0d54af3c30ecb739fa79e1b774f760570e3bd0ea
hubgit720 <n.hamzaoui@afept.fr>
2024-06-21 16:13:20 +0000

    Fix task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,9 +13,11 @@ void jack_bauer(void)
 
 	for (m = 0; m <= 59; m++)
 	{
-	_putchar(h);
+	_putchar(h / 10 + 48);
+	_putchar(h % 10 + 48);
 	_putchar(':');
-	_putchar(m);
+	_putchar(m / 10 + 48);
+	_putchar(m % 10 + 48);
 	_putchar('\n');
 	}
 	}
