32ca151fb3efd4eb4386ba78b707bb250089c70c
hubgit720 <n.hamzaoui@afept.fr>
2024-06-21 08:14:14 +0000

    answer task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,8 +6,11 @@
 */
 void print_alphabet(void)
 {
+	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	_putchar(alphabet);
+	{
+		_putchar(alphabet);
+	}
 	_putchar('\n');
-	return (0);
 }
