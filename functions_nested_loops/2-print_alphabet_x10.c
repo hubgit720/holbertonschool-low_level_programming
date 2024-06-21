03d586ab9f9502fdf4fbfac03f20d15923e1b6ce
hubgit720 <n.hamzaoui@afept.fr>
2024-06-21 10:46:19 +0000

    answer task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * 10 x alphabet - Write a function that prints 10 times the alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+
+	int alphabet_x10;
+	int alphabetcount;
+	
+	for (alphabetcount = 0; alphabetcount < 10; alphabetcount++)
+	{
+		for (alphabet_x10 = 'a'; alphabet_x10 <= 'z'; alphabet_x10++)
+		{
+			_putchar(alphabet_x10);
+		}
+	_putchar('\n');
+	}
+}
