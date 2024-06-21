8cf4682422ef22fb6ffd9d2e15c19db2f6019098
hubgit720 <n.hamzaoui@afept.fr>
2024-06-21 07:49:54 +0000

    test1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-* print_alphabet
+* print_alphabet - Write a function that prints the alphabet
 * Return: 0
 */
 void print_alphabet(void)
 {
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-_putchar
-_putchar('\n');
-return (0);
-
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	_putchar(alphabet);
+	_putchar('\n');
+	return (0);
 }
