b885aa1498e24f2c5d683ad5911de383ffaaa53d
hubgit720 <n.hamzaoui@afept.fr>
2024-06-23 15:25:56 +0000

    answer task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,18 @@
 #include "main.h"
 /**
  * print_last_digit - Write a function that prints the last digit of a number
- *@p: interger
+ *@a: interger
  */
-int print_last_digit(int)
+int print_last_digit(int a)
 {
-	int p;
+	int a;
+	int last_digit;
+
+	for (last_digit = 48; last_digit <= 57; last_digit++)
+	{
+
+	_putchar(last_digit)
+	}
+		return (lastdigit);
+}
+print_last_digit(125)
