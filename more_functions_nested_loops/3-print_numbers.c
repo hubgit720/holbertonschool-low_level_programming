c5664f02c01bedba6c6d2d7707e2540162b3d1be
hubgit720 <n.hamzaoui@afept.fr>
2024-06-24 12:04:32 +0000

    answer task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - Write a function that prints the numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int numbers;
+
+	for (numbers >= '0' && <= '9' numbers++)
